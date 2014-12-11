452a0353ddf846da0abd3f1b0c4460d71fb0bc33
jravi123 <jayashree.ravi.raj@gmail.com>
2014-12-11 13:29:43 -0500

    more test cases

--- app/src/androidTest/java/mbcc/com/testingexampleproject/MainActivityTest.java
+++ app/src/androidTest/java/mbcc/com/testingexampleproject/MainActivityTest.java
@@ -2,19 +2,24 @@ package mbcc.com.testingexampleproject;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
+import org.robolectric.annotation.Config;
 import org.robolectric.util.ActivityController;
 
 import static junit.framework.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(RobolectricGradleTestRunner.class)
+@Config(emulateSdk= 18)
 public class MainActivityTest {
 
     private Activity activity;
@@ -36,7 +41,7 @@ public class MainActivityTest {
     }
 
     @Test
-    public void shouldCreateMainActivityWithATextView() {
+     public void shouldFindTextViewInMainActivity() {
 
         TextView textView = (TextView) activity.findViewById(R.id.textview);
         assertNotNull(textView);
@@ -46,9 +51,25 @@ public class MainActivityTest {
     public void shouldProduceGreetingWhenButtonPressed() {
 
         Button button = (Button) activity.findViewById(R.id.button);
+        assertNotNull(button);
+
         button.performClick();
         TextView textView = (TextView) activity.findViewById(R.id.textview);
         assertTrue(textView.getText().toString().contains(activity.getString(R.string.Greetings)));
+    }
+
+    @Test
+    public void shouldGetCorrectAdapterFromSpinner() {
+
+        Spinner spinner = (Spinner) activity.findViewById(R.id.spinner);
+        assertNotNull(spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(activity,
+                R.array.planets_array, android.R.layout.simple_spinner_item);
+
+        assertEquals(spinner.getAdapter().getCount(), adapter.getCount());
+        assertEquals(spinner.getAdapter().getItem(0), adapter.getItem(0));
+        assertEquals(spinner.getAdapter().getItem(adapter.getCount()-1), adapter.getItem(adapter.getCount()-1));
     }
 
 }
